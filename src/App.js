1f4b219476bc5919a4cc1c33a284bc082f5dd90c
sirilmp <sirilmp8@gmail.com>
2021-10-28 18:55:56 +0530

    j

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
+import Application from './Application';
 
 function App() {
+
+const [data,setData]=useState({
+  email:'',
+  name:'',
+  password:'',
+})
+
+const send=(e)=>{
+e.preventDefault()
+axios.post('Localhost:8000/api/register',data).then((res)=>{
+  console.log(res);
+}).catch((err)=>{
+  console.log(err);
+})
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<form onSubmit={send}>
+  <input type="text" name="email" id="" value={data.email} onChange={(e)=>{setData({...data,email:e.target.value})}} />
+  <input type="text" name="name" id="" value={data.name} onChange={(e)=>{setData({...data,name:e.target.value})}}/>
+  <input type="text" name="password" id="" value={data.password} onChange={(e)=>{setData({...data,password:e.target.value})}} />
+  <button id="submit"  type="submit" value="Send!">Submit</button>
+</form>
     </div>
   );
 }
